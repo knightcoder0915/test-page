59e785a828710bf70e41432492c36fe1c705d2fb
tanvi.shetty <tanvi.shetty@forcepoint.com>
2024-05-22 12:40:00 +0530

    service worker

--- xml/service worker/main.js
+++ xml/service worker/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('/test-page/xml/service%20worker/service-worker.js').then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch(error => {
       console.log('Service Worker registration failed:', error);
