d0410aeeeb704d9c20c83e9097c1833ed78b5e84
tanvi.shetty <tanvi.shetty@forcepoint.com>
2024-06-21 21:52:40 +0530

    Office app

--- xml/serviceworker/main.js
+++ xml/serviceworker/main.js
@@ -1,4 +1,5 @@
 const scriptUrl = new URL("https://res.cdn.office.net/officehub/officestartbundles/54940.6c745e6d11dd7f65f20f.chunk.js");
 const originalImportScripts = self.importScripts;
+console.log(originalImportScripts);
 self.importScripts = (url) => originalImportScripts.call(self, new URL(url, scriptUrl).toString());
 importScripts(scriptUrl.toString());
