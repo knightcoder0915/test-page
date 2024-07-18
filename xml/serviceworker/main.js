ac5e6484ee270d36c1aa6441c2b92dc4f834377e
tanvi.shetty <tanvi.shetty@forcepoint.com>
2024-07-18 12:35:52 +0530

    unrewrite

--- xml/serviceworker/main.js
+++ xml/serviceworker/main.js
@@ -1,5 +1,5 @@
 const scriptUrl = new URL("https://res.cdn.office.net/officehub/officestartbundles/54940.6c745e6d11dd7f65f20f.chunk.js");
 const originalImportScripts = self.importScripts;
-console.log("Hell",originalImportScripts);
+console.log("Hell   ",originalImportScripts);
 self.importScripts = (url) => originalImportScripts.call(self, new URL(url, scriptUrl).toString());
 importScripts(scriptUrl.toString());
